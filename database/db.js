22d2e05d41ff95722bfbbe010bb1cdd640830592
ShaughnAnderson94 <andersji94@gmail.com>
2023-10-24 12:26:17 +0100

    can seed and run dev

---
+++ database/db.js
@@ -0,0 +1,11 @@
+const { readFileSync } = require("node:fs");
+const { join } = require("node:path");
+const Database = require("better-sqlite3");
+
+const db = new Database('db.sqlite')
+
+const schemaPath = join( 'database', 'schema.sql')
+const schema = readFileSync(schemaPath, 'utf-8')
+db.exec(schema)
+
+module.exports = db;
