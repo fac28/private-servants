1555b3a56432326992c74284f42f42bf59e49b7b
isobelbutler <isobutler@icloud.com>
2023-10-24 16:45:34 +0100

    Update seed

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
-const { readFileSync } = require("node:fs");
-const { join } = require("node:path");
-const Database = require("better-sqlite3");
+const { readFileSync } = require('node:fs')
+const { join } = require('node:path')
+const Database = require('better-sqlite3')
 
-const db = new Database("db.sqlite");
+const db = new Database('db.sqlite')
 
-const schemaPath = join("database", "schema.sql");
-const schema = readFileSync(schemaPath, "utf-8");
-db.exec(schema);
+const schemaPath = join('database', 'schema.sql')
+const schema = readFileSync(schemaPath, 'utf-8')
+db.exec(schema)
 
-module.exports = db;
+module.exports = db
