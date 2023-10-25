0b4c16eb649d5b4df6e0884a98989cbae390c9f8
James Sandford Smith <jesandfordsmith@gmail.com>
2023-10-25 14:29:04 +0100

    set up select drop down

---
+++ app/components/VariantSelect.jsx
@@ -0,0 +1,18 @@
+'use client'
+import React, { useState } from 'react'
+import Select from 'react-select'
+
+export default function VariantSelect({ options }) {
+  const [selectedOption, setSelectedOption] = useState(null)
+  return (
+    <Select
+      classNames={{
+        control: (state) =>
+          state.isFocused ? 'border-red-600' : 'border-grey-300',
+      }}
+      options={options}
+      value={selectedOption}
+      onChange={setSelectedOption}
+    ></Select>
+  )
+}
--- app/listings/[id]/page.jsx
+++ app/listings/[id]/page.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { selectServantByID } from '@/lib/model'
+import { listVariants, selectServantByID } from '@/lib/model'
 import Image from 'next/image'
 import AddToCart from '@/app/components/AddToCart'
+import Select from 'react-select'
+import VariantSelect from '@/app/components/VariantSelect'
 
 export default function RenderIndividualServant({ params }) {
   const id = params.id
   const servant = selectServantByID(id)
+  const variants = listVariants()
+  console.log(variants)
+  const options = variants.map((variant) => {
+    return {
+      value: variant.task,
+      label: variant.task,
+    }
+  })
   return (
     <>
       <h3>{servant.name}</h3>
@@ -17,7 +27,10 @@ export default function RenderIndividualServant({ params }) {
         priority
       />
       <p>Price: £{servant.base_price}</p>
+      <VariantSelect options={options} />
+      <p>Total Price: £{servant.base_price}</p>
       <AddToCart />
+
       <p>{servant.about}</p>
     </>
   )
