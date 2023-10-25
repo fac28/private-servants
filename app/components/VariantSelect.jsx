fd597c8a59cb43e7566af9370a9517e08c2d2161
James Sandford Smith <jesandfordsmith@gmail.com>
2023-10-25 15:46:15 +0100

    modify total price by variant

--- app/components/VariantSelect.jsx
+++ app/components/VariantSelect.jsx
@@ -2,18 +2,30 @@
 import React, { useState } from 'react'
 import Select from 'react-select'
 
-export default function VariantSelect({ options }) {
-  const [selectedOption, setSelectedOption] = useState(null)
+export default function VariantSelect({ variants, base_price }) {
+  const [selectedOption, setSelectedOption] = useState({ value: 1 })
+  const options = variants.map((variant) => {
+    return {
+      value: variant.price_modifier,
+      label: variant.task,
+    }
+  })
   return (
-    <Select
-      className="custom-select"
-      classNames={{
-        control: (state) =>
-          state.isFocused ? 'border-red-600' : 'border-grey-300',
-      }}
-      options={options}
-      value={selectedOption}
-      onChange={setSelectedOption}
-    ></Select>
+    <>
+      <Select
+        className="custom-select"
+        classNames={{
+          control: (state) =>
+            state.isFocused ? 'border-red-600' : 'border-grey-300',
+        }}
+        options={options}
+        value={selectedOption}
+        onChange={setSelectedOption}
+      ></Select>
+      <p>
+        Total Price: £
+        {Math.round(base_price * selectedOption.value * 100) / 100}
+      </p>
+    </>
   )
 }
